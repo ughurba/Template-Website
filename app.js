2d2cf1ca8066c41e8c8162df69097be7bdb1e409
ughurba <ughurba@edu.code.az>
2022-05-07 22:15:34 +0400

    create section portfolio

--- app.js
+++ app.js
@@ -3,6 +3,35 @@ const tabsItems = document.querySelectorAll(".info-bottom-img");
 
 tabsBtn.forEach(onTabClick);
 
+const tabFilter = document.querySelectorAll(".porfolio-filter-btn");
+
+tabFilter.forEach(function (item) {
+  item.addEventListener("click", function () {
+    let tabFilters = item;
+    let tabAtributId = tabFilters.getAttribute("data-btn");
+
+    const tabImg = document.getElementsByClassName("portfolio-img-links-info");
+
+    for (var i = 0; i < tabImg.length; i++) {
+      if (tabImg[i].classList.contains(tabAtributId)) { //class name  gore muqayise edir
+   
+        tabImg[i].classList.add("filter-active");
+      } else {
+        tabImg[i].classList.remove("filter-active");
+
+      }
+    }
+
+    tabFilter.forEach(function (item) {
+      item.classList.remove("filter-active");
+    });
+
+
+    tabFilters.classList.add("filter-active");
+   
+  });
+});
+
 function onTabClick(item) {
   tabsBtn.forEach(function (item) {
     item.addEventListener("click", function () {
