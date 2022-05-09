9dba5dfcd2591db764b2174accc9659ea085b7df
ughurba <ughurba@edu.code.az>
2022-05-10 01:26:48 +0400

    create section fag

--- app.js
+++ app.js
@@ -1,36 +1,94 @@
-new Swiper('.swiper-container', {
+new Swiper(".swiper-container", {
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
-    },
-    loop:true,
+  },
+  loop: true,
+
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+
+  // effect:'fade',
+
+  // fadeEffect:{
+  //   crossFade:true,
+  // }
+});
+
+const fagBtns = document.querySelectorAll(".fag-list-in-text");
+const fagItem = document.querySelectorAll(".fag-list-text-in-paragraph");
+const fagDown = document.querySelectorAll(".fa-chevron-down");
+const fagUp = document.querySelectorAll(".fa-chevron-up");
+let isActive =false;
+fagBtns.forEach(function (item) {
+  item.addEventListener("click", function () {
+    var fagBtn = item;
+    var fagId = fagBtn.getAttribute("data-fag");
+    var downId = fagBtn.getAttribute("data-down");
+    var upId = fagBtn.getAttribute("data-up");
+    var fagTabUp = document.querySelector(upId);
+    var fagTab = document.querySelector(fagId);
+    var fagTabDown = document.querySelector(downId);
+
+    fagBtn.addEventListener("click",function(){
+
+      isActive = !isActive;
+      if(isActive){
+        fagItem.forEach(function (item) {
+          item.classList.remove("show-btn-active");
+        });
+        fagBtns.forEach(function (item) {
+          item.classList.remove("show-btn-active");
+        });
+        fagDown.forEach(function (item) {
+          item.classList.remove("show-btn-active");
+        });
+        fagUp.forEach(function (item) {
+          item.classList.remove("show-btn-active");
+        });
+
+      }else{
+
+        fagBtn.classList.add("show-btn-active");
+        fagTab.classList.add("show-btn-active");
+        fagTabDown.classList.add("show-btn-active");
+        fagTabUp.classList.add("show-btn-active");
+      
+      }
+
+    });
 
 
-    autoplay:{
-      delay:2000,
-      stopOnLastSlide:false,
-      disableOnInteraction:false
-    },
-    speed:1000,
 
-    // effect:'fade',
 
-    // fadeEffect:{
-    //   crossFade:true,
-    // }
 
+
+    fagItem.forEach(function (item) {
+      item.classList.remove("show-btn-active");
+    });
+    fagBtns.forEach(function (item) {
+      item.classList.remove("show-btn-active");
+    });
+    fagDown.forEach(function (item) {
+      item.classList.remove("show-btn-active");
+    });
+    fagUp.forEach(function (item) {
+      item.classList.remove("show-btn-active");
+    });
+
+    fagBtn.classList.add("show-btn-active");
+    fagTab.classList.add("show-btn-active");
+    fagTabDown.classList.add("show-btn-active");
+    fagTabUp.classList.add("show-btn-active");
+  });
 });
 
 
-
-
-
-
-const tabsBtn = document.querySelectorAll(".info-bottom-link");
-const tabsItems = document.querySelectorAll(".info-bottom-img");
-
-tabsBtn.forEach(onTabClick);
 
 const tabFilter = document.querySelectorAll(".porfolio-filter-btn");
 
@@ -42,8 +100,8 @@ tabFilter.forEach(function (item) {
     const tabImg = document.getElementsByClassName("portfolio-img-links-info");
 
     for (var i = 0; i < tabImg.length; i++) {
-      if (tabImg[i].classList.contains(tabAtributId)) {    //class name  gore muqayise edir
-     
+      if (tabImg[i].classList.contains(tabAtributId)) {
+       
 
         tabImg[i].classList.add("filter-active");
       } else {
@@ -59,6 +117,10 @@ tabFilter.forEach(function (item) {
   });
 });
 
+const tabsBtn = document.querySelectorAll(".info-bottom-link");
+const tabsItems = document.querySelectorAll(".info-bottom-img");
+
+tabsBtn.forEach(onTabClick);
 
 function onTabClick(item) {
   tabsBtn.forEach(function (item) {
